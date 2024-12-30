fd7ea298f36fd450ff5f5c9748a859a2dacaadbe
spawlov <107248409+spawlov@users.noreply.github.com>
2024-12-30 15:04:20 +0800

    Refactoring: added file parameters

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -1,5 +1,8 @@
 import argparse
+import os
 from argparse import ArgumentParser
+
+from dotenv import find_dotenv, load_dotenv
 
 from handlers import download_image, get_file_extension, get_md5_timestamp
 
@@ -29,7 +32,7 @@ def create_parser() -> ArgumentParser:
     return parser
 
 
-def fetch_spacex_launch(launch_id: str) -> None:
+def fetch_spacex_launch(file_params: dict[str, str], launch_id: str) -> None:
     url = f"https://api.spacexdata.com/v5/launches/{launch_id}"
     with requests.get(url=url, headers=HEADERS, timeout=30) as response:
         response.raise_for_status()
@@ -37,37 +40,45 @@ def fetch_spacex_launch(launch_id: str) -> None:
 
     print(f"Скачивается {len(spacex_links)} фотографий пуска №{response.json()["flight_number"]}")  # noqa
 
-    for link in spacex_links:
+    for i, link in enumerate(spacex_links):
+        filename = f"{file_params["filename"]}_{response.json()["flight_number"]}_{i}"
+        if filename.startswith("random"):
+            filename = get_md5_timestamp(8)
         download_image(
             url=link.strip(),
-            path="images/spacex_temp",
-            filename=f"{get_md5_timestamp(8)}{get_file_extension(link.strip())}",
+            path=file_params["path"],
+            filename=f"{filename}{get_file_extension(link.strip())}",
         )
 
 
-def fetch_spacex_all_launch(limit: int | None = None):
+def fetch_spacex_all_launch(file_params: dict[str, str], limit: int | None = None):
     url = "https://api.spacexdata.com/v5/launches"
     with requests.get(url=url, headers=HEADERS, timeout=30) as response:
         response.raise_for_status()
 
-    indexes = []
+    launch_ids = []
     count_photos = 0
     for launch in reversed(response.json()):
         if launch["links"]["flickr"]["original"]:
-            indexes.append(launch["id"])
+            launch_ids.append(launch["id"])
             count_photos += len(launch["links"]["flickr"]["original"])
 
-    for index in indexes[:limit]:
-        fetch_spacex_launch(index)
+    for launch_id in launch_ids[:limit]:
+        fetch_spacex_launch(file_params, launch_id)
 
 
 def main():
+    load_dotenv(find_dotenv())
+    file_params = {
+        "path": os.getenv("SPACEX_PATH", "images"),
+        "filename": os.getenv("SPACEX_FILENAME", "random"),
+    }
     parser = create_parser()
     namespace = parser.parse_args()
     if namespace.id_launch == "all":
-        fetch_spacex_all_launch(int(namespace.limit))
+        fetch_spacex_all_launch(file_params, int(namespace.limit))
     else:
-        fetch_spacex_launch(namespace.id_launch)
+        fetch_spacex_launch(file_params, namespace.id_launch)
 
 
 if __name__ == "__main__":
