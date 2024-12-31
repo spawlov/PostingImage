1603e44a50c8128b38559fad52d9ee3e144d5713
spawlov <107248409+spawlov@users.noreply.github.com>
2024-12-31 13:24:27 +0800

    publish photo to Telegram channel

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ def main() -> None:
     chanel_id = os.environ["TG_CHANEL_ID"]
 
     bot = telegram.Bot(tg_token)
-    bot.send_message(chat_id=chanel_id, text="Test message for chanel.")
+    # bot.send_message(chat_id=chanel_id, text="Test message for chanel.")
+    bot.send_document(chat_id=chanel_id, document=open("images/spacex/spacex_13_0.jpg", "rb"))
 
 
 if __name__ == "__main__":
