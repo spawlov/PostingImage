c2c31b7d276e4c32080a213b3e56ffc31f2ccb34
spawlov <107248409+spawlov@users.noreply.github.com>
2024-12-31 13:17:39 +0800

    publish text to Telegram channel

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-def main() -> None: ...
+import os
+
+from dotenv import find_dotenv, load_dotenv
+
+import telegram
+
+
+def main() -> None:
+    load_dotenv(find_dotenv())
+    tg_token = os.environ["TG_BOT_TOKEN"]
+    chanel_id = os.environ["TG_CHANEL_ID"]
+
+    bot = telegram.Bot(tg_token)
+    bot.send_message(chat_id=chanel_id, text="Test message for chanel.")
 
 
 if __name__ == "__main__":
